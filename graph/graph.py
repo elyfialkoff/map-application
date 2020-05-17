2623fd83f0581e311e26c3e832303579ab3f6a54
elyfialkoff <mouseen92@gmail.com>
2020-05-16 20:11:37 -0600

    added comments to BFS

--- graph/graph.py
+++ graph/graph.py
@@ -20,8 +20,8 @@ class Graph():
     for v in self.graph:
       print("Adjacency list of vertex {}\n head".format(v), end="")
       nodes = self.graph[v]
-      for city, cost in nodes:
-        print(" -> {} ({})".format(city, cost), end="")
+      for node, cost in nodes:
+        print(" -> {} ({})".format(node.city, cost), end="")
       print("\n")
 
   def BreadthFirstSearch(self, city):
@@ -30,17 +30,24 @@ class Graph():
     for key in self.graph.keys():
       visited[key] = False
 
+    # Queue to keep track of cities that have been found, but not yet visited
     queue = list()
 
+    # Add the first city to the queue, proclaim it visited
     queue.append(city)
-    visited[city]
+    visited[city] = True
 
+    # While there are still cities to visit
     while queue:
+      # remove the first item from the queue
       city = queue.pop(0)
       print(city, end=" ")
 
+      # iterate through its neighboring cities
       for node, _ in self.graph[city]:
         city = node.city
+        # if the city has not yet been visited
         if not visited[city]:
+          # then add it to the queue and proclaim it visited (more like discovered)
           queue.append(city)
           visited[city] = True
